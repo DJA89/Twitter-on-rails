544d62adcc6c8176252e51b9d343e485deae4cd5
Diego Rodríguez <DJA@Diegos-Mac-mini.local>
2015-09-16 13:37:07 -0300

    added unique index to email and made verification case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 
  	validates :email, :presence => true,
                     :format => {:with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i},
-                    :uniqueness => true
+                    :uniqueness => { case_sensitive: false}
 
  	validates_presence_of :first_name
   validates_presence_of :last_name
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150911181626) do
+ActiveRecord::Schema.define(version: 20150916163309) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,8 @@ ActiveRecord::Schema.define(version: 20150911181626) do
     t.string   "last_name"
     t.string   "encrypted_password"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
   add_foreign_key "user_like_tweets", "tweets"
   add_foreign_key "user_like_tweets", "users"
