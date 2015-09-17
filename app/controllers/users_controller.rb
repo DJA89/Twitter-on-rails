bf66abd2059321faa249a265001794fa58c19f8b
Diego Rodríguez <DJA@Diegos-Mac-mini.local>
2015-09-17 16:52:00 -0300

    Finished user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,27 @@ class UsersController < ApplicationController
   end
 
   def new
+    @u = User.new
   end
+
+  def show
+    @u = User.find(params[:id])
+  end
+
+  def create
+    @u = User.new(user_params)
+    if @u.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @u
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,27 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+    it 'should not save user' do
+       user_params = {
+                first_name: "",
+                last_name: "joi",
+                email: "user@example",
+                password: "ere",
+                password_confirmation: "rer"
+              }
+      expect{ post :create, user: user_params }.to_not change{User.count}
+    end
+
+    it 'should sasve user' do
+      user_params = {
+                first_name: FFaker::Name.first_name,
+                last_name: FFaker::Name.last_name,
+                email: FFaker::Internet.email,
+                password: "lararara",
+                password_confirmation: "lararara"
+      }
+
+
+      expect{ post :create, user: user_params }.to change{User.count}.by(1)
+    end
 end
